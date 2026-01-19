1606ffbd42d1ef8362afb5c3aa82fd1fd52a4a24
pakshal-28 <pakshal.briotechno@gmail.com>
2026-01-19 12:46:41 +0530

    Changes

--- src/store/api/stageApi.js
+++ src/store/api/stageApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const stageApi = createApi({
     reducerPath: "stageApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api",
+        baseUrl: import.meta.env.VITE_API_BASE_URL,
         prepareHeaders: (headers) => {
             const token = localStorage.getItem("token");
             if (token) {
