c405ed0f13d4ef112947775c4d494e573da9a6a3
Mahipat <59689193+vaghela983@users.noreply.github.com>
2026-01-22 12:19:57 +0530

    Update Messenger.jsx

--- src/pages/MessengerPart/Messenger.jsx
+++ src/pages/MessengerPart/Messenger.jsx
@@ -13,7 +13,7 @@ import {
   EmptyState,
 } from "../../pages/MessengerPart/MessengerComponents";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 const SOCKET_URL = API_BASE_URL;
 
 export default function MessengerPage() {
